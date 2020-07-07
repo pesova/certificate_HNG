0d1cb14836d5c23df48bb299136dd0c848f9672f
Douglas Okolaa <okolaadouglas@gmail.com>
2020-07-07 17:40:30 +0100

    db schema

---
+++ database/migrations/2020_07_07_162616_create_certificates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('hngi_id')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('total_downloads');
+            $table->integer('downloadable')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}
