d0627fbb22680b2d44fa7f222764013dd7202fae
Douglas Okolaa <okolaadouglas@gmail.com>
2020-07-07 17:18:28 +0100

    demo pdf

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PDF;
+
+class PDFController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function generatePDF()
+    {
+        $data = ['title' => 'Welcome to ItSolutionStuff.com'];
+        $pdf = PDF::loadView('certificates.v1', $data);
+        return $pdf->download('v1.pdf');
+    }
+}
