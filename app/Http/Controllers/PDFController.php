177ee05d4f335bf8394dac3b8fb34e42c14c001a
Douglas Okolaa <okolaadouglas@gmail.com>
2020-07-08 20:14:59 +0100

    fix pdf download

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Setting;
 use App\Certificate;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
 use PDF;
 
 class PDFController extends Controller
@@ -16,9 +18,7 @@ class PDFController extends Controller
      */
     public function generatePDF(Request $request)
     {
-        // dd($request->id);
         $data = Certificate::where('hngi_id', $request->input('id'))->get()->first();
-        // dd($data->version);
         $date = date('d F, Y');
         $settings = Setting::first();
         $data->total_downloads += 1;
@@ -27,10 +27,25 @@ class PDFController extends Controller
         $data->grad_date = $settings->start;
         $data->issued = $date;
 
-        //dd($data->version);
-        //dd($data->version);
-        $data = ['certificate' => $data];
-        $pdf = PDF::loadView('pdfcertificates.v' . $data['certificate']->version, $data)->setPaper('a4', 'landscape');
+        $certificate = ['certificate' => $data];
+
+        $response = Http::withToken(env('PDF_API','B6Oz9M9xQ1gvemLC0ZPVhDAO91TuTtxOaZK1PWB2'))->withOptions([
+            'verify' => false,
+        ])->post('https://docamatic.com/api/v1/pdf', [
+            'source' => URL::to('/certificates/'.$request->input('id')),
+            'format' => 'A4',
+            'media' => 'screen',
+            'landscape' => true
+        ]);
+
+        if ($response->successful()){
+            $data = $response->json();
+            $filename = $certificate['certificate']->version . time() . '.pdf';
+            $temppdf = tempnam(sys_get_temp_dir(), $filename);
+            copy($data['document'], $temppdf);
+            return response()->download($temppdf, $filename);
+        }
+        $pdf = PDF::loadView('certificates.v' . $certificate['certificate']->version, $certificate)->setPaper('a4', 'landscape');
         return $pdf->download('v1.pdf');
     }
 }
