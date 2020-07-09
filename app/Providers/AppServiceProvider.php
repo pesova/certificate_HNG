ba7238544530b9c924213138dd140f04d30404a8
Kofi Mokome <kofimokome10@gmail.com>
2020-07-09 12:40:26 +0100

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (App::environment('production') || App::environment('development')) {
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
     }
